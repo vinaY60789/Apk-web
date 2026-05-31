0bd58c0b5cca03a0aeeba4f8cc24e6cbf24a034a
vinaY60789 <vinaykhatri7895@gmail.com>
2026-05-31 09:22:36 +0530

    Initialize Firebase with configuration settings

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCM-VCdL_Pp6KORiDWMMnWdi_meTaEDGRc",
+  authDomain: "booking-website-b972d.firebaseapp.com",
+  projectId: "booking-website-b972d",
+  storageBucket: "booking-website-b972d.firebasestorage.app",
+  messagingSenderId: "361202710754",
+  appId: "1:361202710754:web:a762de4e25ecdae5ea06bb",
+  measurementId: "G-FJYFCMS27G"
+  firebase.initializeApp(firebaseConfig); const db = firebase.firestore();
+
